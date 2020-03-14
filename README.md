c3ca8383259fd891e59a3e4cff4d53a6766003de
Simon Coong <felix@thestudio-uk.com>
2020-03-14 19:23:23 +0000

    initial commit
    Initial commit of npm project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # botjong
-A Twitch chat bot for organising mahjong games
+A Twitch chatbot for organising riichi mahjong matches
+
+NOTE: for this to be functional, a `.env` file with the parameters `BOT_USERNAME`, `OAUTH_TOKEN`, `CHANNEL_NAME` and `ME` are required in the root of the project. This repo will not have one committed to code for personal data security purposes.
---
+++ bot.js
@@ -0,0 +1,144 @@
+const $tmi = require('tmi.js');
+
+// Define configuration options
+const $opts = {
+    identity: {
+        username: process.env.BOT_USERNAME,
+        password: process.env.OAUTH_TOKEN
+    },
+    channels: [process.env.CHANNEL_NAME]
+};
+
+// Create a client with our options
+const $client = new $tmi.client($opts);
+
+// Connect to Twitch:
+$client.connect();
+
+const isMe = (name) => { return (name === process.env.ME) ? true : false; }
+const getHash = () => Math.random().toString(36).substr(2, 5);
+const findInObject = (obj, name) => {
+    return obj.find(function(e){
+        return (e === name);
+    });
+}
+
+// set up environment object
+let $env = {
+    'waiting': [
+        'danzeeman', 'icarusfw', 'cartesian', 'decinym', 'nasty_wolverine', 'swordfam', 't4n3'
+    ],
+    'playing': [],
+    'tables': {}
+}
+
+const $pervert = [
+    'https://www.reddit.com/r/rule34/',
+    'https://exhentai.org',
+    'https://e-hentai.org',
+    'https://www.pixiv.net/tags/R-18'
+]
+
+const fn = {
+    'join': function(target, data){
+        // sign into waiting list to play
+        $client.say(target, `@${data.$name} has joined the match waiting list.`);
+    },
+    'leave': function(target, data){
+        // sign out of waiting list
+        $client.say(target, `@${data.$name} has left the match waiting list.`);
+    },
+    'waiting': function(target, data){
+        // check waiting lists
+        if (data.$tgt === '-total') {
+            $client.say(target, `BatJong : There are currently ${$env.waiting.length} players awaiting a match.`);
+        } else if (data.$tgt === '-list') {
+            let $temp = '';
+            for (var i = 0; i < $env.waiting.length; i++) {
+                $temp += (i !== 0) ? ', ' : '';
+                $temp += $env.waiting[i];
+            }
+            $client.say(target, `BatJong : Players awaiting a match - ${$temp}`);
+        } else if (data.$tgt === '-tables') {
+            $client.say(target, `BatJong : Hash generated - ${getHash()}`);
+        } else {
+            $client.say(target, `BatJong : I don't recognise that option...`);
+        }
+    },
+    'play': function(target, data){
+        // init game start and move to active tables
+    },
+    'add': function(target, data){
+        // ADMIN ONLY - manually add a player to the waiting list
+        if (data.$me && data.$tgt !== null) {
+            $client.say(target, `BatJong : Oh, hello admin.`);
+        }
+
+        if (data.$me && data.$tgt === null) {
+            $client.say(target, `BatJong : You need to provide a player name to add to the list.`);
+        } 
+
+        if (!data.$me){
+            $client.say(target, `BatJong : That's an admin-only command.`);
+        }
+    },
+    'remove': function(target, data){
+        // ADMIN ONLY - manually remove a player (or all) from the waiting list
+    },
+    'close': function(target, data){
+        // ADMIN ONLY - close a waiting table (or all tables)
+    },
+    'reset': function(target, data){
+        // ADMIN ONLY - reset both wait list and wait tables to init state
+    },
+    'notify': function(target, data){
+        // ADMIN ONLY - send a notification to a waiting table
+    },
+    'start': function(target, data){
+        // ADMIN ONLY - restart command watching
+    },
+    'stop': function(target, data){
+        // ADMIN ONLY - stop command watching (excluding !batjong start)
+    }
+}
+
+// Called every time a message comes in
+function onMessageHandler(target, context, msg, self) {
+    
+    // remove whitespace from chat message and split
+    const $msg = msg.trim().split(" ");
+
+    // compile data object for functions
+    let $data = {
+        $name: context.username,
+        $mod: context.mod,
+        $cmd: $msg[0],
+        $opt: $msg[1] || null,
+        $tgt: $msg[2] || null
+    }
+    // additional personal check
+    $data.$me = isMe($data.$name);
+
+    // Ignore messages from the bot
+    if (self) { return; }
+
+    // if the command is !batjong, execute bot functions
+    if ($data.$cmd === '!batjong' && $data.$opt !== null) { 
+        fn[$data.$opt](target, $data);
+    } else {
+        $client.say(target, `BatJong : I am the night.`);
+    }
+
+    console.log('msg', $msg);
+    console.log('env', $env);
+    console.log('data', $data);
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler(addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
+}
+
+// Register our event handlers (defined below)
+$client.on('message', onMessageHandler);
+$client.on('connected', onConnectedHandler);
